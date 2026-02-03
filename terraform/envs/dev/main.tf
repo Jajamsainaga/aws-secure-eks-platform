466040c660913e0391bda8889128edefc6f86831
Jajamsainaga <k68508880@gmail.com>
2026-02-03 18:12:04 -0500

    Expose VPC outputs in dev environment

--- terraform/envs/dev/main.tf
+++ terraform/envs/dev/main.tf
@@ -4,3 +4,11 @@ module "vpc" {
   environment = "dev"
   aws_region  = var.aws_region
 }
+
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "vpc_cidr" {
+  value = module.vpc.vpc_cidr
+}
